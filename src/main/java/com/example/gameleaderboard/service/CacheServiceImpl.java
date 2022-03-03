0ff07669542aef3192539990e26a62ab8f396214
Harsh Sahu <harsh.sahu@outlook.com>
2022-03-03 21:37:47 +0530

    fixes

--- src/main/java/com/example/gameleaderboard/service/CacheServiceImpl.java
+++ src/main/java/com/example/gameleaderboard/service/CacheServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.gameleaderboard.service;
 
-import com.example.gameleaderboard.controller.ScoreController;
 import com.example.gameleaderboard.model.Score;
 import com.example.gameleaderboard.repository.ScoreRepository;
 import org.slf4j.Logger;
@@ -14,16 +13,14 @@ import java.util.*;
 public class CacheServiceImpl implements CacheService {
 
     private static final Logger logger = LoggerFactory.getLogger(CacheServiceImpl.class);
-    Comparator<Score> scoreComparator = new Comparator<Score>() {
-        @Override
-        public int compare(Score a, Score b) {
-            if (b.getScore() > a.getScore())
-                return -1;
-            else if (b.getScore() < a.getScore())
-                return 1;
-            return 0;
-        }
+    Comparator<Score> scoreComparator = (a, b) -> {
+        if (b.getScore() > a.getScore())
+            return -1;
+        else if (b.getScore() < a.getScore())
+            return 1;
+        return 0;
     };
+
     private final PriorityQueue<Score> minHeap = new PriorityQueue<>(scoreComparator);
     private final HashMap<Long, Score> hashMap = new HashMap<>();
 
@@ -47,27 +44,29 @@ public class CacheServiceImpl implements CacheService {
 
 
     public void addOrUpdateInCache(Score score) {
-        if(hashMap.containsKey(score.getId())){
-            Score old_score = hashMap.get(score.getId());
-            if(old_score.getScore() < score.getScore()){
-                List<Score> scoreList = new LinkedList<>();
-                while(!minHeap.isEmpty()){
-                    Score heapTop = minHeap.poll();
-                    if(heapTop.getId().equals(score.getId()))
-                        scoreList.add(score);
-                    else
-                        scoreList.add(heapTop);
+        try {
+            if (hashMap.containsKey(score.getId())) {
+                Score old_score = hashMap.get(score.getId());
+                if (old_score.getScore() < score.getScore()) {
+                    List<Score> scoreList = new LinkedList<>();
+                    while (!minHeap.isEmpty()) {
+                        Score heapTop = minHeap.poll();
+                        if (heapTop.getId().equals(score.getId()))
+                            scoreList.add(score);
+                        else
+                            scoreList.add(heapTop);
+                    }
+                    minHeap.addAll(scoreList);
+                    hashMap.replace(score.getId(), score);
                 }
-                minHeap.addAll(scoreList);
-                hashMap.replace(score.getId(), score);
+            } else if (minHeap.isEmpty() || minHeap.size() < 5) {
+                addInCache(score);
+            } else if (minHeap.peek().getScore() < score.getScore()) {
+                hashMap.remove(minHeap.poll().getId());
+                addInCache(score);
             }
-        }
-        else if (minHeap.isEmpty() || minHeap.size() < 5) {
-            addInCache(score);
-        }
-        else if (minHeap.peek().getScore() < score.getScore()) {
-            hashMap.remove(minHeap.poll().getId());
-            addInCache(score);
+        } catch (Exception e){
+            logger.error(e.getStackTrace().toString());
         }
     }
 
